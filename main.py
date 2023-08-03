2f7a45dc4378ab5ed844e6b0f5cc9b823ad6e077
themaruf <hasan.m.maruf@gmail.com>
2023-08-03 20:03:34 +0100

    feat: Learn Variables & Data Types

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+# Print Hello World!
+print("Hello World!")
+
+# Variables (Primitive Data Structures)
+# Integer
+age = 35
+# Float
+height = 76.8
+# String
+name = "Jhon Doe"
+# Boolean
+is_male = True
+
+print(name)
+print(age)
+print(height)
+print(is_male)
+

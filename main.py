49348a7f7a2946d0529bd71dbc8defb70e4aa9f4
themaruf <hasan.m.maruf@gmail.com>
2023-08-04 15:59:05 +0100

    feat: learn strings and numbers

--- main.py
+++ main.py
@@ -16,3 +16,26 @@ print(age)
 print(height)
 print(is_male)
 
+# Strings
+phrase = "Hello"
+print(phrase.upper())
+print(phrase.lower())
+print(len(phrase))
+print(phrase[0]) # prints H
+print(phrase.index("H")) # prints 0
+print(phrase.replace("lo", "p"))
+
+#Numbers
+print(10 % 4)
+my_num = -11;
+print(str(my_num) + " is my favourite number.")
+print(abs(my_num))
+print(pow(3,2))
+print(max(4,6))
+print(min(4,6))
+# print(round(3.7))
+
+from math import *
+
+print(floor(3.8))
+print(sqrt(64))
